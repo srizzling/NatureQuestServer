9aa38e8c92cd10068943614673fa8916a5f8eb1a
Sriram Venkatesh <venksriram@gmail.com>
2013-06-02 13:47:31 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nature::Application.config.secret_token = 'd739c7e6e99c658ed86b21d373e5589c9ffbdfaf9b950401a67e9dd400777cdce44258c3b550dbd9026b4945f0025fbf3611ae3d7b8891bdd123a277f2ac56a5'
