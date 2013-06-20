1d84b5b4986489dd9074fb69dc4cd98bac2df1fa
Sriram Venkatesh <venksriram@gmail.com>
2013-06-20 10:55:04 -0400

    Fixed logout issue with by making the link a get function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130615222506) do
+ActiveRecord::Schema.define(:version => 20130620044815) do
 
   create_table "installs", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(:version => 20130615222506) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "authentication_token"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
